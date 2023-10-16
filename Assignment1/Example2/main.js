b17f7a4edfaaaf778645aa8af520216c00824595
Hazem Abolila <hazemabolila44@gmail.com>
2023-10-16 16:45:10 +0300

    Adding Assigment1 Ex(1, 2)

---
+++ Assignment1/Example2/main.js
@@ -0,0 +1,3 @@
+document.write("<h1> hello from js file <h1/>")
+const h1_styling = document.querySelector("h1")
+h1_styling.style.cssText += 'color: blue; font-size: 80px; text-align: center; font-weight: bold; font-family: Arial; text-transform: capitalize';
